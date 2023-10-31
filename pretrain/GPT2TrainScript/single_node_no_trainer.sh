a50730f5e6aae5d1266df330fffb2ddfc1cff53d
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-10-31 09:32:11 +0000

    modify singlenode scripts to run anywhere

--- pretrain/GPT2TrainScript/single_node_no_trainer.sh
+++ pretrain/GPT2TrainScript/single_node_no_trainer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 
 export HF_HOME=/data/s1/jaehwan/nlp-corpus/.cache/huggingface ### this is the directory path of huggingface cache (default: ~/.cache/huggingface)
-DISTRIBUTED_ARGS="--nnodes=1 --nproc_per_node=4 --node-rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=192.168.0.133:1234" ### node IP
+DISTRIBUTED_ARGS="--nnodes=1 --nproc_per_node=4 --node-rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=localhost:1234" ### node IP
 export OMP_NUM_THREADS=16
 
 
 CUDA_VISIBLE_DEVICES="0,1,2,3" \
-MASTER_ADDR=d3 \
+MASTER_ADDR=localhost \
 torchrun $DISTRIBUTED_ARGS ../run_clm_no_trainer.py \
     --config_name gpt2 \
-    --tokenizer_name gpt2 \
+    --tokenizer_name ../../tokenizers/bml-test \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
     --num_train_epochs 1 \
--- pretrain/GPT2TrainScript/single_node_trainer.sh
+++ pretrain/GPT2TrainScript/single_node_trainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-export HF_HOME=/data/s1/jaehwan/nlp-corpus/.cache/huggingface ### this is the directory path of huggingface cache (default: ~/.cache/huggingface)
-DISTRIBUTED_ARGS="--nnodes=1 --nproc_per_node=4 --node-rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=d2:1234" # node IP
+# export HF_HOME=/data/s1/jaehwan/nlp-corpus/.cache/huggingface ### this is the directory path of huggingface cache (default: ~/.cache/huggingface)
+DISTRIBUTED_ARGS="--nnodes=1 --nproc_per_node=4 --node-rank=0 --rdzv_id=123 --rdzv_backend=c10d --rdzv_endpoint=localhost:1234" # node IP
 export OMP_NUM_THREADS=16
 
 ### Tokenization
@@ -22,14 +22,14 @@ EVAL_SAVE_ITER=2000
 CUDA_VISIBLE_DEVICES="0,1,2,3" \
 torchrun $DISTRIBUTED_ARGS ../run_clm.py \
     --config_name $MODEL \
-    --tokenizer_name ../test.txt \
+    --tokenizer_name ../../tokenizers/bml-test \
     --train_file /data/s1/chanwoo/required/$DATASET.json \
     --token False \
     --do_train \
     --do_eval \
     --num_train_epochs $NUM_EPOCH \
     --per_device_train_batch_size $LOCAL_BATCH_SIZE \
-    --output_dir /data/s1/jaehwan/nlp-corpus/gpt2-train-logs \
+    --output_dir logs \
     --overwrite_output_dir \
     --ddp_timeout 18000 \
     --skip_memory_metrics False \
