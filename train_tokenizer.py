e25d64849e3264eb0a2be95a83783f9ba8d440db
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-10-31 08:25:38 +0000

    use same process with skt/kogpt

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,6 +1,14 @@
 from argparse import ArgumentParser
 from transformers import AutoTokenizer
 from datasets import load_dataset
+from tokenizers import (
+    Tokenizer,
+    decoders,
+    models,
+    normalizers,
+    pre_tokenizers,
+    trainers,
+)
 
 parser = ArgumentParser()
 parser.add_argument("--data-file", type=str, required=True, help="Data file to train")
@@ -15,5 +23,28 @@ old_tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 corpus = (x["text"] for x in dataset["train"])
 
-tokenizer = old_tokenizer.train_new_from_iterator(corpus, 52000)
-tokenizer.save_pretrained(args.tokenizer_name)
+
+tokenizer = Tokenizer(
+    models.BPE(
+        bos_token="<|endoftext|>", eos_token="<|endoftext|>", unk_token="<|endoftext|>"
+    )
+)
+normalizer = normalizers.Sequence(
+    [
+        normalizers.NFKC(),
+        normalizers.BertNormalizer(
+            clean_text=False,
+            handle_chinese_chars=False,
+            strip_accents=False,
+            lowercase=False,
+        ),
+    ]
+)
+tokenizer.normalizer = normalizer
+
+tokenizer.pre_tokenizer = pre_tokenizers.Metaspace()
+tokenizer.decoder = decoders.Metaspace()
+trainer = trainers.BpeTrainer(vocab_size=51200)
+
+tokenizer.train_from_iterator(corpus, trainer=trainer)
+tokenizer.save(args.tokenizer_name)
