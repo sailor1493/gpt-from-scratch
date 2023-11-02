384466e870e2e8327b794d786eefd16df59c4886
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-11-02 12:32:05 +0000

    move tokenizer train script into pretrain dir

--- pretrain/train_tokenizer.py
+++ pretrain/train_tokenizer.py
@@ -1,5 +1,4 @@
 from argparse import ArgumentParser
-from transformers import AutoTokenizer
 from datasets import load_dataset
 from tokenizers import (
     Tokenizer,
@@ -8,7 +7,9 @@ from tokenizers import (
     normalizers,
     pre_tokenizers,
     trainers,
+    AddedToken,
 )
+from transformers import PreTrainedTokenizerFast
 
 parser = ArgumentParser()
 parser.add_argument("--data-file", type=str, required=True, help="Data file to train")
@@ -19,16 +20,16 @@ args = parser.parse_args()
 
 dataset = load_dataset("json", data_files=args.data_file)
 
-old_tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 corpus = (x["text"] for x in dataset["train"])
 
-
-tokenizer = Tokenizer(
-    models.BPE(
-        bos_token="<|endoftext|>", eos_token="<|endoftext|>", unk_token="<|endoftext|>"
-    )
+to_add = AddedToken(
+    "<|endoftext|>", single_word=False, lstrip=False, rstrip=False, normalized=True
 )
+tokenizer = Tokenizer(
+    models.BPE(),
+)
+tokenizer.add_special_tokens([to_add])
 normalizer = normalizers.Sequence(
     [
         normalizers.NFKC(),
@@ -44,7 +45,19 @@ tokenizer.normalizer = normalizer
 
 tokenizer.pre_tokenizer = pre_tokenizers.Metaspace()
 tokenizer.decoder = decoders.Metaspace()
-trainer = trainers.BpeTrainer(vocab_size=51200)
+trainer = trainers.BpeTrainer(vocab_size=50257, special_tokens=[to_add])
 
 tokenizer.train_from_iterator(corpus, trainer=trainer)
-tokenizer.save(args.tokenizer_name)
+# tokenizer.save(args.tokenizer_name)
+
+
+tok_to_push = PreTrainedTokenizerFast(
+    model_max_length=1024,
+    bos_token=to_add,
+    eos_token=to_add,
+    unk_token=to_add,
+    clean_up_tokenization_spaces=True,
+    add_prefix_space=False,
+    tokenizer_object=tokenizer,
+)
+tok_to_push.save_pretrained(args.tokenizer_name)
