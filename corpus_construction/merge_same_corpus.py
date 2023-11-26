9db4f2bd6da0875ab55aa64ed66fb8c4d9a3b784
Chanwoo Park <chanwoo@aces.snu.ac.kr>
2023-11-26 16:00:37 +0000

    progress: make mixup corpus

--- corpus_construction/merge_same_corpus.py
+++ corpus_construction/merge_same_corpus.py
@@ -7,7 +7,7 @@ parser.add_argument("--pttn", type=str, required=True)
 parser.add_argument("--name", type=str, required=True)
 args = parser.parse_args()
 
-base_dir = "nlp_project/corpus"
+base_dir = "nlp_project/corpus/yna/roseanne"
 tgt_file = f"nlp_project/workspace/{args.name}.jsonl"
 
 file_paths = [
---
+++ scripts/make_mixup_corpus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+python corpus_construction/sample_corpus.py \
+    --src corpus_construction/nlp_project/experiment_corpus/bulk_books_shuffled.json \
+    --tgt corpus_construction/nlp_project/experiment_corpus/bulk_books_for_mixup.json \
+    --ratio 0.25
+python corpus_construction/sample_corpus.py \
+    --src corpus_construction/nlp_project/experiment_corpus/specialized.json \
+    --tgt corpus_construction/nlp_project/experiment_corpus/specialized_for_mixup.json \
+    --ratio 0.25
+python corpus_construction/sample_corpus.py \
+    --src corpus_construction/nlp_project/experiment_corpus/newspaper.json \
+    --tgt corpus_construction/nlp_project/experiment_corpus/newspaper_for_mixup.json \
+    --ratio 0.5
+
+touch corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json
+cat corpus_construction/nlp_project/experiment_corpus/bulk_books_for_mixup.json >> corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json
+cat corpus_construction/nlp_project/experiment_corpus/specialized_for_mixup.json >> corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json
+cat corpus_construction/nlp_project/experiment_corpus/newspaper_for_mixup.json >> corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json
+
+python corpus_construction/shuffle_corpus.py \
+    --src corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json \
+    --tgt corpus_construction/nlp_project/experiment_corpus/mixup_corpus_shuffled.json
+
+# remove intermediate files
+rm corpus_construction/nlp_project/experiment_corpus/bulk_books_for_mixup.json
+rm corpus_construction/nlp_project/experiment_corpus/specialized_for_mixup.json
+rm corpus_construction/nlp_project/experiment_corpus/newspaper_for_mixup.json
+rm corpus_construction/nlp_project/experiment_corpus/mixup_corpus.json
